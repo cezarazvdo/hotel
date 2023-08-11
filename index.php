cc3f3710a872a9f2800a73fb7bd8cc0e6ee51bcf
cezarazvdo <cezardiazevedo@gmail.com>
2023-08-11 16:39:01 -0300

    feat: exbindo primeiras informacoes

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-// Silence is golden.
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+    </head>
+    <body>
+        <?php get_header() ?>
+        <?php get_footer() ?>
+    </body>
+</html>

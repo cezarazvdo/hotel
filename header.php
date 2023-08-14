c38f1243a1624a433fd3c9050e8f31ef3555870a
cezarazvdo <cezardiazevedo@gmail.com>
2023-08-14 19:28:14 -0300

    feat: adicionado header

--- header.php
+++ header.php
@@ -1,11 +1,46 @@
 <?php
-function titulo($titulo) {
-    echo $titulo;
-}
-
+    $titulo = 'Divergente';
+    $logo = get_template_directory_uri().'/assets/logo_divergente.png';
 ?>
 
+<style>
+    .header {
+        background-color: black;
+        display: flex;
+        flex-direction: row;
+        height: 80px;
+        width: 100%;
+        justify-content: space-between;
+    }
+    .title {
+        color: white;
+    }
+    .list {
+        display: flex;
+        flex-direction: row;
+        color: white;
+        list-style: none;
+        gap: 20px;
+        padding: 0;
+        font-family: "Roboto", sans-serif;
+        margin-right: 100px;
+    }
 
-<section class="top-bar">
-    <h1><?php titulo('test') ?></h1>
-</section>
+    .containerList {
+        align-self: center;
+    }
+</style>
+
+<nav class="header">
+    <div class="logo">
+        <img src="<?php echo $logo ?>" alt="logo" />
+    </div>
+    <div class="containerList">
+        <ul class="list">
+            <li><h1>Bem Vindo</h1></li>
+            <li><h1>Quartos</h1></li>
+            <li><h1>Restaurante</h1></li>
+            <li><h1>Fale Conosco</h1></li>
+        </ul>
+    </div> 
+</nav>
